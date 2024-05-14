345e51260794bc575fccf1b73d8b58c37c7c5672
valepazg <valeria.paz.gutierrez@gmail.com>
2024-05-14 19:12:27 -0400

    bug escala inventario

--- scenes/BlueBox.gd
+++ scenes/BlueBox.gd
@@ -5,16 +5,17 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var box_scale_x = (viewport.size.x/5) / box.texture.get_width()
-	var  box_scale_y = (viewport.size.x/5) / box.texture.get_height()
-	box.scale.x = box_scale_x
-	box.scale.y = box_scale_y
-	
-	box.global_position.x += (viewport.size.x/5)/2
-	box.global_position.y += (viewport.size.x/5)/2
-	
-	orange_box.global_position.y = (viewport.size.y - box.texture.get_height()/2 * box_scale_y)
-	print('orange_box', orange_box.global_position.y)
+	pass
+	#var box_scale_x = (viewport.size.x/5) / box.texture.get_width()
+	#var  box_scale_y = (viewport.size.x/5) / box.texture.get_height()
+	#box.scale.x = box_scale_x
+	#box.scale.y = box_scale_y
+	#
+	##box.global_position.x += (viewport.size.x/5)/2
+	##box.global_position.y += (viewport.size.x/5)/2
+	#
+	##orange_box.global_position.y = (viewport.size.y - box.texture.get_height()/2 * box_scale_y)
+	#print('orange_box', orange_box.global_position.y)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
