f2fe4c1ae162379ac6fcf963e8c9b9fab877b87f
DiegoEGarciaG <daigogar@gmail.com>
2024-05-19 01:49:10 -0400

    Modified bullets functioning to implement piece placement on entering the correct areas of the puzzle

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 
+@export var player_group = 'null'
+
+func _ready():
+	add_to_group(player_group)
+
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		handle_collision(collision)
 
-
 func handle_collision(collision : KinematicCollision2D):
 	velocity = velocity.bounce(collision.get_normal())
+	
+
+func set_group(group):
+	player_group = group
