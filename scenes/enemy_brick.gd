f4cf8e79326b4aba3cd365259561a1c13f673f58
valepazg <valeria.paz.gutierrez@gmail.com>
2024-05-25 01:40:54 -0400

    queue free projectiles

--- scenes/enemy_brick.gd
+++ scenes/enemy_brick.gd
@@ -8,6 +8,9 @@ var positioned = false
 var base_size = Vector2i(960, 540) 
 var piece_size = base_size * (0.6/PuzzleSettings.PUZZLE_PIECES)
 var delta_counter = 0
+var shot_intervals = [0, 3, 6, 9]  
+var directions = [Vector2.UP, Vector2.DOWN, Vector2.RIGHT, Vector2.LEFT]  
+
 func _ready():
 	var new_brick_size = piece_size / 2
 	var brick_texture_size = sprite.texture.get_size()
@@ -18,15 +21,14 @@ func _ready():
 	sprite.centered = false
 	collision_shape_2d.position = Vector2(sprite.texture.get_size() * sprite.scale / 2)
 
-
 func _process(delta):
-	
 	if positioned:
-		delta_counter += 1
-		if delta_counter >= 250:
-			var projectile = load("res://scenes/projectile.tscn").instantiate()
-			#projectile.scale *= 4
-			projectile.set_velocity(global_position.direction_to(Vector2(0,0)) * 200)
-			projectile.global_position = global_position
-			add_child(projectile)
+		delta_counter += delta
+		if delta_counter >= 5:
 			delta_counter = 0
+			var directions = [Vector2(1, 0), Vector2(-1, 0), Vector2(0, 1), Vector2(0, -1)]
+			for direction in directions:
+				var projectile = load("res://scenes/projectile.tscn").instantiate()
+				projectile.set_velocity(direction * 200)
+				projectile.global_position = global_position
+				add_child(projectile)
